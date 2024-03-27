2ce94489ce66bd266b8dccfe8ac4364011433bb2
arekstasko <arek.stasko@gmail.com>
2024-03-27 21:18:01 +0100

    Add repository and logger injection

--- IdentityProviderSystem.API/IdentityProviderSystem.Domain.Services/UserService/UserService.cs
+++ IdentityProviderSystem.API/IdentityProviderSystem.Domain.Services/UserService/UserService.cs
@@ -1,11 +1,22 @@
 ﻿using IdentityProviderSystem.Domain.DTO;
 using IdentityProviderSystem.Domain.Models.User;
+using IdentityProviderSystem.Persistance.Repositories.UserRepository;
 using LanguageExt.Common;
+using Microsoft.Extensions.Logging;
 
 namespace IdentityProviderSystem.Domain.Services.UserService;
 
 public class UserService : IUserService
 {
+    private readonly IUserRepository _repository;
+    private readonly ILogger<IUserService> _logger;
+    
+    public UserService(IUserRepository repository, ILogger<IUserService> logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+    
     public async Task<Result<bool>> Register(UserDTO user)
     {
         throw new NotImplementedException();

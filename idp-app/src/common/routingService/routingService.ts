5d894b618e304ec97f37f1f48baa784a641442a2
arekstasko <arek.stasko@gmail.com>
2024-07-02 12:41:13 +0200

    Update docker-compose

--- idp-app/src/common/routingService/routingService.ts
+++ idp-app/src/common/routingService/routingService.ts
@@ -4,5 +4,6 @@ export const returnToBaseSite = (userId: string, token: string) => {
     const returnSite = GetReturnSite();
     if(!returnSite) return;
     const url = `${returnSite}/${userId}/${token}`
+    console.log(url)
     window.location.href = url;
 }

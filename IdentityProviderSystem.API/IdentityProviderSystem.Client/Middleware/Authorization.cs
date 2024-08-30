ac0f3fa6bd380cb9b6565330b4d6c67935352d36
ArekStasko <arek.stasko@gmail.com>
2024-08-30 19:59:31 +0200

    Add IdentityProviderSystem client auth middleware

---
+++ IdentityProviderSystem.API/IdentityProviderSystem.Client/Middleware/Authorization.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using IdentityProviderSystem.Client.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityProviderSystem.Client.Middleware
+{
+    public class Authorization
+    {
+        private readonly RequestDelegate _next;
+        private readonly ITokenService _tokenService;
+
+        public Authorization(RequestDelegate next, ITokenService tokenService, ILogger<Authorization> logger)
+        {
+            _next = next;
+            _tokenService = tokenService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"].ToString();
+            if (string.IsNullOrEmpty(token))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
+            var tokenValidationResult = await _tokenService.ValidateToken(token);
+            if (!tokenValidationResult.IsTokenValid)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
+
+            context.Items["UserId"] = tokenValidationResult.UserId;
+            await _next(context);
+        }
+    }
+
+}
